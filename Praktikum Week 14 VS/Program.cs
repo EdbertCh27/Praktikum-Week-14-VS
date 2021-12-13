de78b57917f1bfc5a332bf30b2fa35d8e5ff83d2
EdbertCh27 <edbertch27@gmail.com>
2021-12-13 10:56:43 +0800

    Praktikum (gitnya sempat eror)

---
+++ Praktikum Week 14 VS/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace Praktikum_Week_14_VS
+{
+    class Program
+    {
+        static void DeretPrima(int angka)
+        {
+            //DERET PRIMA
+            Console.WriteLine("Output : ");
+            var hitung = 1;
+            for (int i = 1; i <= angka; i++)
+            {
+                for (int j = 1; j <= angka; j++)
+                {
+                    while (true)
+                    {
+                        hitung++;
+                        int cekFaktor = 1;
+                        int cek2 = 1;
+                        while (cekFaktor != 0)
+                        {
+                            cek2++;
+                            cekFaktor = hitung % cek2;
+                        }
+                        if (hitung == cek2)
+                        {
+                            break;
+                        }
+                    }
+                    Console.Write("{0,7}", hitung);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void DeretNonPrima(int angka)
+        {
+            //DERET NON PRIMA
+            Console.WriteLine("Output : ");
+            var hitung = 0;
+            for (int i = 1; i <= angka; i++)
+            {
+                for (int j = 1; j <= angka; j++)
+                {
+                    while (true)
+                    {
+                        hitung++;
+                        int cekFaktor = 1;
+                        int cek2 = 1;
+                        while (cekFaktor != 0)
+                        {
+                            cek2++;
+                            cekFaktor = hitung % cek2;
+                        }
+                        if (hitung != cek2)
+                        {
+                            break;
+                        }
+                    }
+                    Console.Write("{0,7}", hitung);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            //CEK PRIMA INPUT
+            Console.Write("Input : ");
+            int angka = Convert.ToInt32(Console.ReadLine());
+            int faktor = 0;
+            for (int i = 1; i <= angka; i++)
+            {
+                if (angka % i == 0)
+                {
+                    faktor++;
+                }
+            }
+            if (faktor == 2)
+            {
+                DeretPrima(angka);
+            }
+            else
+            {
+                DeretNonPrima(angka);
+            }
+        }
+    }
+}
